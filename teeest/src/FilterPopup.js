543c2593c63ff0b108604736cdccd4d94490da73
Andy <cherniakhovych.andy@gmail.com>
2024-11-27 12:16:18 +0200

    added data to the table and added styles for them

---
+++ teeest/src/FilterPopup.js
@@ -0,0 +1,106 @@
+import React, { useState, useEffect, useRef } from "react";
+
+
+import "./FilterPopup.css";
+
+
+const FilterPopup = ({ position, onClose }) => {
+  const [searchTerm, setSearchTerm] = useState("");
+  const popupRef = useRef(null);
+
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (popupRef.current && !popupRef.current.contains(event.target)) {
+        onClose(); // Викликаємо onClose, якщо клік був поза попапом
+      }
+    };
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [onClose]);
+
+  return (
+    <div
+      ref={popupRef}
+      className="popup"
+      style={{
+        top: `${position.top}px`,
+        left: `${position.left}px`,
+        position: "absolute",
+        backgroundColor: "white",
+        zIndex: 10,
+      }}
+    >
+      <p className="filter-popup-button" onClick={onClose}>
+        Sort A to Z
+      </p>
+      <p className="filter-popup-button" onClick={onClose}>
+        Sort Z to A
+      </p>
+      <div className="popup-search">
+        <input
+          type="text"
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+          placeholder="Search"
+          className="popup-search-input"
+        />
+      </div>
+
+      <div className="filter-popup-selection">
+        <p className="filter-popup-selection-selected">7 selected</p>
+        <div className="filter-popup-selection-container">
+          <p className="filter-popup-selection-select">Select:</p>
+          <p className="filter-popup-selection-ALL">ALL</p>
+          <p className="filter-popup-selection-NONE">NONE</p>
+        </div>
+      </div>
+
+      <div className="filter-popup-line"></div>
+
+      <div className="filter-popup-checkboxes">
+        <div className="checkbox-section-container">
+          <label className="checkbox-container">
+            <input type="checkbox" className="checkbox" />
+            <span className="checkmark"></span>
+          </label>
+          <p className="answer-option">On track</p>
+        </div>
+        <div className="checkbox-section-container">
+          <label className="checkbox-container">
+            <input type="checkbox" className="checkbox" />
+            <span className="checkmark"></span>
+          </label>
+          <p className="answer-option">Catching up</p>
+        </div>
+        <div className="checkbox-section-container">
+          <label className="checkbox-container">
+            <input type="checkbox" className="checkbox" />
+            <span className="checkmark"></span>
+          </label>
+          <p className="answer-option">Falling behind</p>
+        </div>
+        <div className="checkbox-section-container">
+          <label className="checkbox-container">
+            <input type="checkbox" className="checkbox" />
+            <span className="checkmark"></span>
+          </label>
+          <p className="answer-option">N/A</p>
+        </div>
+      </div>
+
+      <div className="filter-popup-line" style={{ marginTop: "35px" }}></div>
+      <div className="filter-popup-actions">
+        <button className="filter-popup-button-discard" onClick={onClose}>
+          Discard
+        </button>
+        <button className="filter-popup-button-apply" onClick={onClose}>
+          Apply
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default FilterPopup;
--- teeest/src/ListOfHomeworks.js
+++ teeest/src/ListOfHomeworks.js
@@ -1,26 +1,37 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import axios from "axios";
+
+import FilterPopup from "./FilterPopup";
 
 import logo from "./img/Logo.svg";
 import buttonRight from "./img/list-of-homeworks-button-right.svg";
 import buttonLeft from "./img/list-of-homeworks-button-left.svg";
-import filterIcon from "./img/filter-icon.svg";
 import lockIcon from "./img/lock-icon.svg";
 import "./ListOfHomeworks.css";
+import "./FilterPopup.css";
 
 const API_BASE_URL =
   "http://127.0.0.1:5001/aylee-learns-english-dev/us-central1/api/api";
 
+
 const ListOfHomeworks = () => {
-  const [homeworks, setHomeworks] = useState([]); 
-  const [searchTerm, setSearchTerm] = useState(""); 
-  const [loading, setLoading] = useState(true); 
+  const [homeworks, setHomeworks] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+  const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
-  const [currentPage, setCurrentPage] = useState(1); 
-  const itemsPerPage = 10; 
+  const [currentPage, setCurrentPage] = useState(1);
+  const itemsPerPage = 10;
 
- 
-
+  const [popupVisible, setPopupVisible] = useState(false);
+  const [popupPosition, setPopupPosition] = useState({ top: 0, left: 0 });
+  const handleFilterClick = (e, columnName) => {
+    const rect = e.target.getBoundingClientRect(); // Отримуємо координати елемента
+    setPopupPosition({
+      top: rect.bottom + window.scrollY, // Нижня межа елемента + відступ прокрутки
+      left: rect.left + window.scrollX, // Ліва межа елемента + відступ прокрутки
+    });
+    setPopupVisible(true); // Відкриваємо попап
+  };
 
   const fetchHomeworks = async () => {
     try {
@@ -28,28 +39,28 @@ const ListOfHomeworks = () => {
         `${API_BASE_URL}/homework?homeworkIds=1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22`
       );
       setHomeworks(response.data.homeworks);
-      setLoading(false); 
+      setLoading(false);
     } catch (error) {
       console.error("Error fetching homeworks:", error);
-      setError("Failed to load data."); 
+      setError("Failed to load data.");
       setLoading(false);
     }
   };
 
   const formatDate = (dateString) => {
     const date = new Date(dateString);
-    return date.toLocaleString('en-US', {
-      year: 'numeric',
-      month: 'long',
-      day: 'numeric',
-      hour: 'numeric',
-      minute: 'numeric',
-      hour12: true
+    return date.toLocaleString("en-US", {
+      year: "numeric",
+      month: "long",
+      day: "numeric",
+      hour: "numeric",
+      minute: "numeric",
+      hour12: true,
     });
   };
 
   useEffect(() => {
-    fetchHomeworks(); 
+    fetchHomeworks();
   }, []);
 
   const filteredHomeworks = homeworks.filter((hw) => {
@@ -60,12 +71,15 @@ const ListOfHomeworks = () => {
     const matchesDate =
       searchLower.length >= 3 &&
       (formatDate(hw.createdAt).toLowerCase().includes(searchLower) ||
-       formatDate(hw.dueDate).toLowerCase().includes(searchLower));
+        formatDate(hw.dueDate).toLowerCase().includes(searchLower));
     return matchesTitle || matchesDate;
   });
 
   const startIndex = (currentPage - 1) * itemsPerPage;
-  const currentItems = filteredHomeworks.slice(startIndex, startIndex + itemsPerPage);
+  const currentItems = filteredHomeworks.slice(
+    startIndex,
+    startIndex + itemsPerPage
+  );
 
   // Обробка кнопок пагінації
   const handleNextPage = () => {
@@ -110,7 +124,9 @@ const ListOfHomeworks = () => {
 
             <div className="content-pagination">
               <p className="content-pagination-info">
-                {startIndex + 1}-{Math.min(startIndex + itemsPerPage, filteredHomeworks.length)} of {filteredHomeworks.length}
+                {startIndex + 1}-
+                {Math.min(startIndex + itemsPerPage, filteredHomeworks.length)}{" "}
+                of {filteredHomeworks.length}
               </p>
               <img
                 src={buttonLeft}
@@ -135,26 +151,90 @@ const ListOfHomeworks = () => {
             ) : (
               <table className="homeworks-table">
                 <thead>
-                <tr>
-                  <th id="table-text">Status </th>
-                  <th id="table-text">Avaible On:</th>
-                  <th id="table-text">Due On:</th>
-                  <th id="table-text">Class <img src={lockIcon} className={"lock-icon"} alt="lolockIcongo" style={{ width: "9px" }} /></th>
-                  <th id="table-text">Homework Type</th>
-                  <th id="table-text">Complation Rate</th>
-                </tr>
+                  <tr>
+                    <th
+                      id="table-text"
+                      onClick={(e) => handleFilterClick(e, "status")}
+                    >
+                      Status
+                    </th>
+                    <th
+                      id="table-text"
+                      onClick={(e) => handleFilterClick(e, "createdAt")}
+                    >
+                      Available On
+                    </th>
+                    <th
+                      id="table-text"
+                      onClick={(e) => handleFilterClick(e, "dueDate")}
+                    >
+                      Due On
+                    </th>
+                    <th id="table-text">
+                      Class{" "}
+                      <img
+                        src={lockIcon}
+                        className={"lock-icon"}
+                        alt="lolockIcongo"
+                        style={{ width: "9px" }}
+                      />
+                    </th>
+                    <th
+                      id="table-text"
+                      onClick={(e) => handleFilterClick(e, "dueDate")}
+                    >
+                      Homework Type
+                    </th>
+                    <th
+                      id="table-text"
+                      onClick={(e) => handleFilterClick(e, "dueDate")}
+                    >
+                      Complation Rate
+                    </th>
+                  </tr>
                 </thead>
                 <tbody>
                   {currentItems.map((hw) => (
                     <tr key={hw.id}>
-                      <td>{hw.id}</td>
+                      <td>
+                        <button
+                          className={`status-${hw.status
+                            .toLowerCase()
+                            .replace(/\s+/g, "-")}`}
+                        >
+                          {hw.status}
+                        </button>
+                      </td>
+
                       <td>{formatDate(hw.createdAt)}</td>
                       <td>{formatDate(hw.dueDate)}</td>
-                      <td>{hw.subject}</td>
+                      <td>{hw.class}</td>
+                      <td>{hw.type}</td>
+                      <td>
+                        <button
+                          className={
+                            hw.rate >= 0 && hw.rate <= 30
+                              ? "rate-completion-low"
+                              : hw.rate >= 31 && hw.rate <= 80
+                              ? "rate-completion-middle"
+                              : hw.rate >= 81 && hw.rate <= 100
+                              ? "rate-completion-high"
+                              : ""
+                          }
+                        >
+                          {hw.rate}%
+                        </button>
+                      </td>
                     </tr>
                   ))}
                 </tbody>
               </table>
+            )}
+            {popupVisible && (
+              <FilterPopup
+                position={popupPosition}
+                onClose={() => setPopupVisible(false)}
+              />
             )}
           </div>
         </div>
